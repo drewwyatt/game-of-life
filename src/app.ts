3485db155a08c8b5e68ec519f4e61a8efec3e7b3
Drew Wyatt <drew.j.wyatt@gmail.com>
2019-01-29 23:20:38 -0500

    Added underpopulation

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { Application } from 'pixi.js'
 import { Grid } from './grid'
-import { createRuleset, noUnderpopulatedCells } from './rules'
+import { createRuleset, noUnderpopulatedCells, noOverpopulatedCells } from './rules'
 
 // Add grid to screen
 const app = new Application(0, 0, { backgroundColor: 0x1099bb })
@@ -15,8 +15,18 @@ grid
   .toggle(19)
   .toggle(34)
   .toggle(42)
+  .toggle(60)
+  .toggle(61)
+  .toggle(62)
+  .toggle(75)
+  .toggle(76)
+  .toggle(77)
+  .toggle(100)
+  .toggle(101)
+  .toggle(115)
+  .toggle(116)
 
 // Start loop
-setInterval(createRuleset(grid, [noUnderpopulatedCells]), 1000)
+setInterval(createRuleset(grid, [noUnderpopulatedCells, noOverpopulatedCells]), 1000)
 
 export default app
--- src/rules.ts
+++ src/rules.ts
@@ -1,3 +1,4 @@
+import { gt, lt } from 'sanctuary'
 import { Cell } from './cell'
 import { Grid } from './grid'
 
@@ -6,13 +7,18 @@ const apply = <A, B extends (a: A) => any>(a: A) => (b: B) => b(a)
 type Mutation = [Cell[], Cell[]]
 type Rule = (grid: Grid) => Mutation
 
+const filterOnNeighborLength = (grid: Grid, predicate: (length: number) => boolean): Cell[] =>
+  grid.livingCells.filter(({ index }) => predicate(grid.getLivingNeighbors(index).length))
+
 /**
  * Any live cell with fewer than two live neighbors dies, as if by underpopulation.
  */
-export const noUnderpopulatedCells: Rule = grid => [
-  grid.livingCells.filter(cell => grid.getLivingNeighbors(cell.index).length < 2),
-  []
-]
+export const noUnderpopulatedCells: Rule = grid => [filterOnNeighborLength(grid, lt(2)), []]
+
+/**
+ * Any live cell with more than three live neighbors dies, as if by overpopulation.
+ */
+export const noOverpopulatedCells: Rule = grid => [filterOnNeighborLength(grid, gt(3)), []]
 
 export const createRuleset = (grid: Grid, rules: Rule[]) => () => {
   rules.map(apply(grid)).forEach(([toKill, toRes]: Mutation) => {
@@ -22,6 +28,5 @@ export const createRuleset = (grid: Grid, rules: Rule[]) => () => {
 }
 
 // TODO:
-// Any live cell with two or three live neighbors lives on to the next generation.
 // Any live cell with more than three live neighbors dies, as if by overpopulation.
 // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
